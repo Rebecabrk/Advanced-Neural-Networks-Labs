f14f570649c72e665705ea7f5297d67126111d6d
Rebecabrk <rebeca.costache@yahoo.com>
2025-11-17 17:45:08 +0200

    updated to work on colab and on local

--- pytorch-training-pipeline/utils/constants.py
+++ pytorch-training-pipeline/utils/constants.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 import yaml
+import os
 
-DEFAULT_CONFIG_PATH = Path("/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/default_config.yaml")
+# assuming that the script is always run from 
+# /Advanced-Neural-Networks-Labs/pytorch-training-pipeline
+ROOT = os.getcwd() 
+DEFAULT_CONFIG_PATH = Path(ROOT + "/configs/default_config.yaml")
 
 VALID_DATASETS = ["MNIST", "CIFAR-10", "CIFAR-100", "OxfordIIITPet"]
 VALID_MODELS = ["resnet18", "resnet50", "resnest14d", "resnest26d", "MLP"]
@@ -9,21 +13,21 @@ VALID_OPTIMIZERS = ["SGD", "Adam", "AdamW", "Muon", "SAM"]
 VALID_SCHEDULERS = ["StepLR", "ReduceLROnPlateau"]
 
 with open(
-    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/optimizers.yaml", 
+    ROOT  + "/configs/optimizers.yaml", 
     "r") as f:
     OPTIMIZER_PARAMS = yaml.safe_load(f)
 
 with open(
-    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/transforms.yaml",
+    ROOT + "/configs/transforms.yaml",
       "r") as f:
     TRANSFORM_PRESETS = yaml.safe_load(f)
 
 with open(
-    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/MLP.yaml",
+    ROOT + "/configs/MLP.yaml",
       "r") as f:
     MLP_PARAMS = yaml.safe_load(f)
     
 with open(
-    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/schedulers.yaml",
+    ROOT + "/configs/schedulers.yaml",
       "r") as f:
     SCHEDULER_PARAMS = yaml.safe_load(f)
