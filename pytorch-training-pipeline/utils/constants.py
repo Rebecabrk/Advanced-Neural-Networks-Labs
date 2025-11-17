d63f0857d55dcef7b6498f5d4d5e1307210b25b5
Rebecabrk <rebeca.costache@yahoo.com>
2025-11-17 17:06:12 +0200

    prepared path for Google Colab

--- pytorch-training-pipeline/utils/constants.py
+++ pytorch-training-pipeline/utils/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import yaml
 
-DEFAULT_CONFIG_PATH = Path("/Users/rebecacostache/Desktop/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/default_config.yaml")
+DEFAULT_CONFIG_PATH = Path("/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/default_config.yaml")
 
 VALID_DATASETS = ["MNIST", "CIFAR-10", "CIFAR-100", "OxfordIIITPet"]
 VALID_MODELS = ["resnet18", "resnet50", "resnest14d", "resnest26d", "MLP"]
@@ -9,21 +9,21 @@ VALID_OPTIMIZERS = ["SGD", "Adam", "AdamW", "Muon", "SAM"]
 VALID_SCHEDULERS = ["StepLR", "ReduceLROnPlateau"]
 
 with open(
-    "/Users/rebecacostache/Desktop/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/optimizers.yaml", 
+    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/optimizers.yaml", 
     "r") as f:
     OPTIMIZER_PARAMS = yaml.safe_load(f)
 
 with open(
-    "/Users/rebecacostache/Desktop/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/transforms.yaml",
+    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/transforms.yaml",
       "r") as f:
     TRANSFORM_PRESETS = yaml.safe_load(f)
 
 with open(
-    "/Users/rebecacostache/Desktop/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/MLP.yaml",
+    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/MLP.yaml",
       "r") as f:
     MLP_PARAMS = yaml.safe_load(f)
     
 with open(
-    "/Users/rebecacostache/Desktop/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/schedulers.yaml",
+    "/content/Advanced-Neural-Networks-Labs/pytorch-training-pipeline/configs/schedulers.yaml",
       "r") as f:
     SCHEDULER_PARAMS = yaml.safe_load(f)
